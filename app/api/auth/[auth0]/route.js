e6709daa51bbe4666b4cda1105d9adea6bc116a4
Balkishun Maurya <140256426+Bkishun@users.noreply.github.com>
2024-09-24 16:07:16 +0530

    24/09/2024

--- app/api/auth/[auth0]/route.js
+++ app/api/auth/[auth0]/route.js
@@ -1,11 +1,11 @@
-import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
+import { handle, handleLogin } from "@0/nextjs-0";
 
-export const GET = handleAuth({
+export const GET = handle({
   login: handleLogin({
     returnTo: "/",
   }),
   signup: handleLogin({
-    authorizationParams: {
+    orizationParams: {
       screen_hint: "signup",
     },
     returnTo: "/onboarding",
